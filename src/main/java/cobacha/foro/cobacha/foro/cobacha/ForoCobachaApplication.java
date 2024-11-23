c27bb6d6b731b0d5d3f1c4e2c9223b57521cf9f2
hugo <hugo.alarcon@icorp.com.mx>
2024-11-22 19:28:10 -0600

    Initial commit

---
+++ src/main/java/cobacha/foro/cobacha/foro/cobacha/ForoCobachaApplication.java
@@ -0,0 +1,13 @@
+package cobacha.foro.cobacha.foro.cobacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoCobachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoCobachaApplication.class, args);
+	}
+
+}
