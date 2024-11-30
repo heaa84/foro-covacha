bf87340f987104fea09fe3ad517f047061d52011
hugo <hugo.alarcon@icorp.com.mx>
2024-11-29 19:46:45 -0600

    Coriguiendo relacio de tabla topico curso

--- src/main/java/cobacha/foro/domain/topico/DatosRegistroTopicoConCurso.java
+++ src/main/java/cobacha/foro/domain/topico/DatosRegistroTopicoConCurso.java
@@ -7,7 +7,7 @@ public record DatosRegistroTopicoConCurso(
             @NotBlank(message = "El título del tópico no puede estar vacío") String titulo,
             @NotBlank(message = "El mensaje del tópico no puede estar vacío") String mensaje,
             @NotBlank(message = "El autor del topico no puede estar vacío") String autor,
-            @NotBlank String nombre, // nombre del curso / por ejemplo java
-            @NotBlank String categoria // categoria del curso / por ejemplo Lenguaje de proguamacion
+            @NotBlank String nombre, // nombre del curso / Operaciones Basicas java
+            @NotBlank String categoria // categoria del curso / Java
 ){
 }
