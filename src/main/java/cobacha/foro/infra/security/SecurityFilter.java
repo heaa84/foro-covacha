8886d54fd8275a686c68e42e4197f64a3b82c9c6
hugo <hugo.alarcon@icorp.com.mx>
2024-12-03 17:51:49 -0600

    Aplicando filtro SecurityFilter

--- src/main/java/cobacha/foro/infra/security/SecurityFilter.java
+++ src/main/java/cobacha/foro/infra/security/SecurityFilter.java
@@ -1,4 +1,19 @@
 package cobacha.foro.infra.security;
 
-public class SecurityFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+/* Filtro de token*/
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Elfiltro esta ciendo llamando");
+    }
 }
