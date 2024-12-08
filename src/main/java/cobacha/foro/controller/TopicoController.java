2467ee25f26c230f2977c46a1fb3d59167658ba8
hugo <hugo.alarcon@icorp.com.mx>
2024-12-08 06:40:16 -0600

    Asignar usuario al autor del topico

--- src/main/java/cobacha/foro/controller/TopicoController.java
+++ src/main/java/cobacha/foro/controller/TopicoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+
 import java.util.Optional;
 
 
