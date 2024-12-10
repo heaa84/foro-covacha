86d2f20fe2fd50191c4783d1f29478e41f11b7be
hugo <hugo.alarcon@icorp.com.mx>
2024-12-09 21:05:04 -0600

    UsuarioController Lista de Usuarios

--- src/main/java/cobacha/foro/domain/usuarios/DatosRegistrarNuevoUsuario.java
+++ src/main/java/cobacha/foro/domain/usuarios/DatosRegistrarNuevoUsuario.java
@@ -1,4 +1,19 @@
 package cobacha.foro.domain.usuarios;
 
-public record DatosRegistrarNuevoUsuario() {
+import cobacha.foro.domain.usuario.Usuario;
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosRegistrarNuevoUsuario(
+        @NotBlank
+        String nombre,
+        @NotBlank
+                String correo_electronico,
+        @NotBlank
+        String contrasena,
+        @NotBlank
+        String perfil
+) {
+    public DatosRegistrarNuevoUsuario (Usuario usuario){
+        this(usuario.getNombre(), usuario.getCorreo_electronico(), usuario.getContrasena(), usuario.getPerfil());
+    }
 }
