a7ec436e6be6028bfea96470a27ef53ec92e6a33
hugo <hugo.alarcon@icorp.com.mx>
2024-12-13 07:04:34 -0600

    Corrección de nombre

--- src/test/java/covacha/foro/ForoCovachaApplicationTests.java
+++ src/test/java/covacha/foro/ForoCovachaApplicationTests.java
@@ -1,10 +1,10 @@
-package covacha.foro.cobacha.foro.cobacha;
+package covacha.foro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ForoCobachaApplicationTests {
+class ForoCovachaApplicationTests {
 
 	@Test
 	void contextLoads() {
