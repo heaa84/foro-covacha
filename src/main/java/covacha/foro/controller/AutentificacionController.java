ef938402a49804ce3f46c00519ab9f4c9ef6cde3
HugoAA <hugo.alarcon@icorp.com.mx>
2024-12-14 17:52:36 -0600

    Update AutentificacionController.java
    add comentarios y corrigiendo el nombre de la variable: usuarioAtentificacion

--- src/main/java/covacha/foro/controller/AutentificacionController.java
+++ src/main/java/covacha/foro/controller/AutentificacionController.java
@@ -24,7 +24,6 @@ public class AutentificacionController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
-;;;
     @Autowired
     private TokenService tokenService;
 
@@ -33,9 +32,17 @@ public class AutentificacionController {
             summary = "Autentificar usuario",
             description = "Autentificar usuario, por default nombre: admin, contraseña: admin")
     public ResponseEntity autentificacionUsuario(@RequestBody @Valid DatosAtuentificacionUsuario datosAtuentificacionUsuario){
+        // crea variable AuthToken a partir de una clase UsernamePasswordAuthenticationToken(datosAtuentificacionUsuario.nombre(),datosAtuentificacionUsuario.contrasena()) 
+        // Esto no crea un nuevo objeto de tipo Authentication 
         Authentication AuthToken =new UsernamePasswordAuthenticationToken(datosAtuentificacionUsuario.nombre(),datosAtuentificacionUsuario.contrasena());
-        var usuarioAutenticado=authenticationManager.authenticate(AuthToken);
-        var JWTToken=tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        // Usamos usuarioAutenticado=authenticationManager.authenticate(AuthToken) donde, mandamos AuthToken como parametro si el metodo consige autenticar los datos,
+        // Devuelve un objeto con los datos de la autentificación,
+        // Y lo guardamos en usuarioAutentificado
+        var usuarioAutentificado=authenticationManager.authenticate(AuthToken);
+        // El método tokenService.generarToken nos ayuda a generar el JWT, tenemos que pasarle el usuario principal ya autentificado.
+        // lo guardamos en JWTToken
+        var JWTToken=tokenService.generarToken((Usuario) usuarioAutentificado.getPrincipal());
+        // Retornamos el token en formato JSON
         return ResponseEntity.ok(new DatosJWTToken(JWTToken));
     }
 }
