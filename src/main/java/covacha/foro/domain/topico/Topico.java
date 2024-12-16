2af2f4f738b9b1b29225a895510e357e6de49791
hugo <hugo.alarcon@icorp.com.mx>
2024-12-15 19:15:26 -0600

    modificando topico paraa que tenga relacion con respuesta

--- src/main/java/covacha/foro/domain/topico/Topico.java
+++ src/main/java/covacha/foro/domain/topico/Topico.java
@@ -1,6 +1,7 @@
 package covacha.foro.domain.topico;
 
 import covacha.foro.domain.curso.Curso;
+import covacha.foro.domain.respuesta.Respuesta;
 import covacha.foro.domain.topico.dto.DatosActualizarTopico;
 import covacha.foro.domain.topico.dto.DatosRegistroTopicoConCurso;
 import jakarta.persistence.*;
@@ -10,6 +11,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Table(name = "topicos")
@@ -41,6 +44,9 @@ public class Topico {
     @ManyToOne
     @JoinColumn(name = "curso_id", nullable = false) // Asegúrate de que este nombre de columna coincida con tu esquema de base de datos
     private Curso curso;
+    // Relacion con respuesta
+    @OneToMany(mappedBy = "topico", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<Respuesta> respuestas = new ArrayList<>();
 
     // Constructor para inicializar usando un objeto de tipo DatosRegistroTopico
     public Topico(@Valid DatosRegistroTopicoConCurso datosRegistroTopicoConCurso) {
@@ -63,6 +69,17 @@ public class Topico {
             this.autor = datosActualizarTopico.autor();
         }
 
+    }
+
+    //Métodos para respuesta
+    public void addRespuesta(Respuesta respuesta) {
+        respuesta.setTopico(this);
+        this.respuestas.add(respuesta);
+    }
+
+    public void removeRespuesta(Respuesta respuesta) {
+        respuesta.setTopico(null);
+        this.respuestas.remove(respuesta);
     }
 }
 
